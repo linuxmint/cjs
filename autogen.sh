e54cc8ad48cad2cac11bc9dd7174309aec7bb664
Philip Chimento <philip.chimento@gmail.com>
2016-09-26 20:12:39 -0700

    build: Modernize autogen.sh
    This gets rid of the dependency on the old gnome-autogen.sh and moves
    instead to the suggested autogen.sh given as an example in the
    gnome-common migration guide:
    https://wiki.gnome.org/Projects/GnomeCommon/Migration
    
    https://bugzilla.gnome.org/show_bug.cgi?id=772027

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,15 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-test -n "$srcdir" || srcdir=$(dirname "$0")
-test -n "$srcdir" || srcdir=.
-
-olddir=$(pwd)
-
-cd $srcdir
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -z "$srcdir" && srcdir=.
+olddir=`pwd`
+cd "$srcdir"
 
 (test -f configure.ac) || {
-    echo "*** ERROR: Directory '$srcdir' does not look like the top-level project directory ***"
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level directory"
     exit 1
 }
-
-# shellcheck disable=SC2016
-PKG_NAME=$(autoconf --trace 'AC_INIT:$1' configure.ac)
 
 if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
     echo "*** WARNING: I am going to run 'configure' with no arguments." >&2
@@ -22,16 +18,15 @@ if [ "$#" = 0 -a "x$NOCONFIGURE" = "x" ]; then
     echo "" >&2
 fi
 
-mkdir -p m4
-
+aclocal --install || exit 1  # autoreconf will do this automatically in future
 autoreconf --verbose --force --install || exit 1
 
 cd "$olddir"
 if [ "$NOCONFIGURE" = "" ]; then
-    $srcdir/configure "$@" || exit 1
+    "$srcdir/configure" "$@" || exit 1
 
     if [ "$1" = "--help" ]; then exit 0 else
-        echo "Now type 'make' to compile $PKG_NAME" || exit 1
+        echo "Now type 'make' to compile" || exit 1
     fi
 else
     echo "Skipping configure process."
