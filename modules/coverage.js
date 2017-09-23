ab577b2f71aee97ae9791a83b1d49c54e7d8b301
Philip Chimento <philip.chimento@gmail.com>
2017-09-22 22:52:49 -0700

    coverage: Don't error out on various files
    The coverage statistics should handle files with #! on the first line,
    and ignore files with names within angle brackets, which is our
    convention for scripts that are fed to the JS engine programmatically.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=788166

--- modules/coverage.js
+++ modules/coverage.js
@@ -757,6 +757,9 @@ function _fetchCountersFromCache(filename, cache, nLines) {
 }
 
 function _fetchCountersFromReflection(filename, contents, nLines) {
+    // Shebang is illegal character sequence to JS parser
+    if (contents.startsWith('#!'))
+        contents = '//' + contents;
     let reflection = Reflect.parse(contents);
     let functions = functionsForAST(reflection);
 
@@ -795,6 +798,9 @@ function CoverageStatisticsContainer(prefixes, cache) {
     }
 
     function ensureStatisticsFor(filename) {
+        // Skip scripts fed to JS engine programmatically.
+        if (filename.startsWith('<') && filename.endsWith('>'))
+            return undefined;
         if (!coveredFiles[filename])
             coveredFiles[filename] = createStatisticsFor(filename);
         return coveredFiles[filename];
