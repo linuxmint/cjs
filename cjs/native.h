GNOME/gjs

context: Always import byteArray
Previously, it could happen in some rare cases that we try to marshal a
GByteArray into JS before the byteArray module is imported. That would
cause a crash. To prevent this, always import byteArray.

Closes #39.

gjs/native.h
@@ -42,10 +42,10 @@ bool     gjs_is_registered_native_module(JSContext  *context,
                                          JSObject   *parent,
                                          const char *name);
 
-/* called by importer.c to load a statically linked native module */
-bool gjs_import_native_module (JSContext              *context,
-                               const char             *name,
-                               JS::MutableHandleObject module_out);
+/* called by importer.cpp to load a statically linked native module */
+bool gjs_load_native_module(JSContext              *cx,
+                            const char             *name,
+                            JS::MutableHandleObject module);
 
 G_END_DECLS
 

linuxmint/cjs

cjs/native.h
@@ -42,10 +42,10 @@ bool     gjs_is_registered_native_module(JSContext  *context,
                                          JSObject   *parent,
                                          const char *name);
 
-/* called by importer.c to load a statically linked native module */
-bool gjs_import_native_module (JSContext              *context,
-                               const char             *name,
-                               JS::MutableHandleObject module_out);
+/* called by importer.cpp to load a statically linked native module */
+bool gjs_load_native_module(JSContext              *cx,
+                            const char             *name,
+                            JS::MutableHandleObject module_out);
 
 G_END_DECLS
 