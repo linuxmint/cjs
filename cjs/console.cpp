GNOME/gjs

profiler: Implement --profile flag behaviour
This changes the behaviour of the profiler command line flag to what was
discussed in #31. --profile without an argument starts the profiler and
dumps to gjs-$PID.syscap in the current directory, and --profile=foo.dat
starts the profiler and dumps to foo.dat.

gjs/console.cpp
@@ -36,14 +36,21 @@ static char *coverage_output_path = NULL;
 static char *profile_output_path = nullptr;
 static char *command = NULL;
 static gboolean print_version = false;
+static gboolean enable_profiler = false;
 
+static gboolean parse_profile_output(const char *, const char *, void *, GError **);
+
+/* Keep in sync with entries in check_script_args_for_stray_gjs_args() */
 static GOptionEntry entries[] = {
     { "version", 0, 0, G_OPTION_ARG_NONE, &print_version, "Print GJS version and exit" },
     { "command", 'c', 0, G_OPTION_ARG_STRING, &command, "Program passed in as a string", "COMMAND" },
     { "coverage-prefix", 'C', 0, G_OPTION_ARG_STRING_ARRAY, &coverage_prefixes, "Add the prefix PREFIX to the list of files to generate coverage info for", "PREFIX" },
     { "coverage-output", 0, 0, G_OPTION_ARG_STRING, &coverage_output_path, "Write coverage output to a directory DIR. This option is mandatory when using --coverage-path", "DIR", },
     { "include-path", 'I', 0, G_OPTION_ARG_STRING_ARRAY, &include_path, "Add the directory DIR to the list of directories to search for js files.", "DIR" },
-    { "profile-output", 0, 0, G_OPTION_ARG_FILENAME, &profile_output_path, "Enable the profiler and Write output to FILE", "FILE" },
+    { "profile", 0, G_OPTION_FLAG_OPTIONAL_ARG | G_OPTION_FLAG_FILENAME,
+        G_OPTION_ARG_CALLBACK, reinterpret_cast<void *>(&parse_profile_output),
+        "Enable the profiler and write output to FILE (default: gjs-$PID.syscap)",
+        "FILE" },
     { NULL }
 };
 
@@ -86,6 +93,31 @@ strcatv(char **strv1,
     return retval;
 }
 
+static gboolean
+parse_profile_output(const char *option_name,
+                     const char *value,
+                     void       *data,
+                     GError    **error_out)
+{
+    enable_profiler = true;
+    if (value)
+        profile_output_path = g_strdup(value);
+    return true;
+}
+
+static gboolean
+check_stray_profile_arg(const char *option_name,
+                        const char *value,
+                        void       *data,
+                        GError    **error_out)
+{
+    g_warning("You used the --profile option after the script on the GJS "
+              "command line. Support for this will be removed in a future "
+              "version. Place the option before the script.");
+    g_free(profile_output_path);
+    return parse_profile_output(option_name, value, data, error_out);
+}
+
 static void
 check_script_args_for_stray_gjs_args(int           argc,
                                      char * const *argv)
@@ -94,10 +126,13 @@ check_script_args_for_stray_gjs_args(int           argc,
     char **new_coverage_prefixes = NULL;
     char *new_coverage_output_path = NULL;
     char **new_include_paths = NULL;
+    /* Keep in sync with entries[] at the top */
     static GOptionEntry script_check_entries[] = {
         { "coverage-prefix", 'C', 0, G_OPTION_ARG_STRING_ARRAY, &new_coverage_prefixes },
         { "coverage-output", 0, 0, G_OPTION_ARG_STRING, &new_coverage_output_path },
         { "include-path", 'I', 0, G_OPTION_ARG_STRING_ARRAY, &new_include_paths },
+        { "profile", 0, G_OPTION_FLAG_OPTIONAL_ARG | G_OPTION_FLAG_FILENAME,
+          G_OPTION_ARG_CALLBACK, reinterpret_cast<void *>(&check_stray_profile_arg) },
         { NULL }
     };
     char **argv_copy = g_new(char *, argc + 2);
@@ -278,8 +313,11 @@ main(int argc, char **argv)
     /* Allow SIGUSR2 (with sigaction param) to enable/disable */
     gjs_profiler_setup_signals();
 
-    if (profile_output_path) {
-        gjs_profiler_set_filename(profiler, profile_output_path);
+    if (enable_profiler) {
+        if (profile_output_path) {
+            gjs_profiler_set_filename(profiler, profile_output_path);
+            g_free(profile_output_path);
+        }
         gjs_profiler_start(profiler);
     }
 

linuxmint/cjs

cjs/console.cpp
@@ -36,14 +36,21 @@ static char *coverage_output_path = NULL;
 static char *profile_output_path = nullptr;
 static char *command = NULL;
 static gboolean print_version = false;
+static gboolean enable_profiler = false;
 
+static gboolean parse_profile_arg(const char *, const char *, void *, GError **);
+
+/* Keep in sync with entries in check_script_args_for_stray_gjs_args() */
 static GOptionEntry entries[] = {
     { "version", 0, 0, G_OPTION_ARG_NONE, &print_version, "Print GJS version and exit" },
     { "command", 'c', 0, G_OPTION_ARG_STRING, &command, "Program passed in as a string", "COMMAND" },
     { "coverage-prefix", 'C', 0, G_OPTION_ARG_STRING_ARRAY, &coverage_prefixes, "Add the prefix PREFIX to the list of files to generate coverage info for", "PREFIX" },
     { "coverage-output", 0, 0, G_OPTION_ARG_STRING, &coverage_output_path, "Write coverage output to a directory DIR. This option is mandatory when using --coverage-path", "DIR", },
     { "include-path", 'I', 0, G_OPTION_ARG_STRING_ARRAY, &include_path, "Add the directory DIR to the list of directories to search for js files.", "DIR" },
-    { "profile-output", 0, 0, G_OPTION_ARG_FILENAME, &profile_output_path, "Enable the profiler and Write output to FILE", "FILE" },
+    { "profile", 0, G_OPTION_FLAG_OPTIONAL_ARG | G_OPTION_FLAG_FILENAME,
+        G_OPTION_ARG_CALLBACK, reinterpret_cast<void *>(&parse_profile_arg),
+        "Enable the profiler and write output to FILE (default: gjs-$PID.syscap)",
+        "FILE" },
     { NULL }
 };
 
@@ -86,6 +93,31 @@ strcatv(char **strv1,
     return retval;
 }
 
+static gboolean
+parse_profile_arg(const char *option_name,
+                  const char *value,
+                  void       *data,
+                  GError    **error_out)
+{
+    enable_profiler = true;
+    if (value)
+        profile_output_path = g_strdup(value);
+    return true;
+}
+
+static gboolean
+check_stray_profile_arg(const char *option_name,
+                        const char *value,
+                        void       *data,
+                        GError    **error_out)
+{
+    g_warning("You used the --profile option after the script on the GJS "
+              "command line. Support for this will be removed in a future "
+              "version. Place the option before the script.");
+    g_free(profile_output_path);
+    return parse_profile_arg(option_name, value, data, error_out);
+}
+
 static void
 check_script_args_for_stray_gjs_args(int           argc,
                                      char * const *argv)
@@ -94,10 +126,13 @@ check_script_args_for_stray_gjs_args(int           argc,
     char **new_coverage_prefixes = NULL;
     char *new_coverage_output_path = NULL;
     char **new_include_paths = NULL;
+    /* Keep in sync with entries[] at the top */
     static GOptionEntry script_check_entries[] = {
         { "coverage-prefix", 'C', 0, G_OPTION_ARG_STRING_ARRAY, &new_coverage_prefixes },
         { "coverage-output", 0, 0, G_OPTION_ARG_STRING, &new_coverage_output_path },
         { "include-path", 'I', 0, G_OPTION_ARG_STRING_ARRAY, &new_include_paths },
+        { "profile", 0, G_OPTION_FLAG_OPTIONAL_ARG | G_OPTION_FLAG_FILENAME,
+          G_OPTION_ARG_CALLBACK, reinterpret_cast<void *>(&check_stray_profile_arg) },
         { NULL }
     };
     char **argv_copy = g_new(char *, argc + 2);
@@ -278,8 +313,11 @@ main(int argc, char **argv)
     /* Allow SIGUSR2 (with sigaction param) to enable/disable */
     gjs_profiler_setup_signals();
 
-    if (profile_output_path) {
-        gjs_profiler_set_filename(profiler, profile_output_path);
+    if (enable_profiler) {
+        if (profile_output_path) {
+            gjs_profiler_set_filename(profiler, profile_output_path);
+            g_free(profile_output_path);
+        }
         gjs_profiler_start(profiler);
     }
 