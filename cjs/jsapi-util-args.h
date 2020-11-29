9c56fcf767442c49875f625114dd7cdb91a068f9
Leigh Scott <leigh123linux@gmail.com>
2020-11-29 09:52:01 +0000

    fix build with clang

--- cjs/jsapi-util-args.h
+++ cjs/jsapi-util-args.h
@@ -43,7 +43,7 @@
 #include "cjs/jsapi-util.h"
 #include "cjs/macros.h"
 
-GJS_ALWAYS_INLINE [[nodiscard]] static inline bool check_nullable(
+[[nodiscard]] GJS_ALWAYS_INLINE static inline bool check_nullable(
     const char*& fchar, const char*& fmt_string) {
     if (*fchar != '?')
         return false;
