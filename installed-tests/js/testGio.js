96d507c903ea1b2244c5360fb29bf276d627f5df
Michael Webster <miketwebster@gmail.com>
2023-01-19 20:40:49 -0500

    Fix GSettings tests.

--- installed-tests/js/testGio.js
+++ installed-tests/js/testGio.js
@@ -110,13 +110,13 @@ describe('Gio.Settings overrides', function () {
     });
 
     it("doesn't crash when forgetting to specify a schema path", function () {
-        expect(() => new Gio.Settings({schema: 'org.gnome.GjsTest.Sub'}))
+        expect(() => new Gio.Settings({schema: 'org.gnome.CjsTest.Sub'}))
             .toThrowError(/schema/);
     });
 
     it("doesn't crash when specifying conflicting schema paths", function () {
         expect(() => new Gio.Settings({
-            schema: 'org.gnome.GjsTest',
+            schema: 'org.gnome.CjsTest',
             path: '/conflicting/path/',
         })).toThrowError(/schema/);
     });
@@ -127,7 +127,7 @@ describe('Gio.Settings overrides', function () {
         let settings;
 
         beforeEach(function () {
-            settings = new Gio.Settings({schema: 'org.gnome.GjsTest'});
+            settings = new Gio.Settings({schema: 'org.gnome.CjsTest'});
         });
 
         it("doesn't crash when resetting a nonexistent key", function () {
