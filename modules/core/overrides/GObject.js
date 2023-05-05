GNOME/gjs

GObject: Handle versions of GLib without GBindingGroup
We don't require GLib 2.72 for anything else, so it makes sense to
conditionally define the override for GObject.BindingGroup.bind_full().
If GLib is too old then the override (and its tests) are skipped.

modules/core/overrides/GObject.js
@@ -691,9 +691,11 @@ function _init() {
         return GjsPrivate.g_object_bind_property_full(this, ...args);
     };
 
-    GObject.BindingGroup.prototype.bind_full = function (...args) {
-        return GjsPrivate.g_binding_group_bind_full(this, ...args);
-    };
+    if (GObject.BindingGroup !== undefined) {
+        GObject.BindingGroup.prototype.bind_full = function (...args) {
+            return GjsPrivate.g_binding_group_bind_full(this, ...args);
+        };
+    }
 
     // fake enum for signal accumulators, keep in sync with gi/object.c
     GObject.AccumulatorType = {

linuxmint/cjs

modules/core/overrides/GObject.js
@@ -689,9 +689,11 @@ function _init() {
         return CjsPrivate.g_object_bind_property_full(this, ...args);
     };
 
-    GObject.BindingGroup.prototype.bind_full = function (...args) {
-        return CjsPrivate.g_binding_group_bind_full(this, ...args);
-    };
+    if (GObject.BindingGroup !== undefined) {
+        GObject.BindingGroup.prototype.bind_full = function (...args) {
+            return CjsPrivate.g_binding_group_bind_full(this, ...args);
+        };
+    }
 
     // fake enum for signal accumulators, keep in sync with gi/object.c
     GObject.AccumulatorType = {